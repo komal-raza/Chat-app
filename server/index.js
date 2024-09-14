a2bca3922e1c6e4f23b69f695058fdb8082fdb58
razaakomal <komal.raza@leoatech.com>
2024-09-14 07:26:13 +0500

    fixed static file path error

--- server/index.js
+++ server/index.js
@@ -5,23 +5,25 @@ const messageRoutes = require("./routes/message.routes.js");
 const authRoutes = require("./routes/auth.routes.js");
 const userRoutes = require("./routes/user.routes.js");
 const cookieParser = require("cookie-parser");
-const {  server, app } = require("./socket/socket.js");
-const path = require("path")
+const { server, app } = require("./socket/socket.js");
+const path = require("path");
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());  //parse JSON payload from request body
+app.use(express.json()); //parse JSON payload from request body
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/messages", messageRoutes);
 app.use("/api/v1/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// Serve static files from the frontend dist folder
+app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
 
+// Catch-all route to serve the frontend's index.html
 app.get("*", (req, res) => {
-	res.sendFile(path.join( __dirname,"frontend", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
 });
 server.listen(PORT, () => {
   connectDB();
